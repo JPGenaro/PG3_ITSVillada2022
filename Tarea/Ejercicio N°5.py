7aaf623c8bd23163d07f66b20f91817a57ccb443
JPGenaro <71389486+JPGenaro@users.noreply.github.com>
2022-04-08 09:55:06 -0300

    Delete Tarea directory

--- Tarea/Ejercicio N°5.py
+++
@@ -1,9 +0,0 @@
-def ejercicio_5():
-    print("==Ejercicio N°5==")
-    texto: str = input("Ingrese la palabra: ")
-    if str(texto) == str(texto)[::-1]:
-        print(f"{texto} = True")
-    else:
-        print(f"{texto} = False")
-
-ejercicio_5()
