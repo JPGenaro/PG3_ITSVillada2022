f7a016f040ef65aea015ad761c05dd5a49497d4a
Juan Pablo Genaro <jpgenaro06@gmail.com>
2022-05-06 08:11:58 -0300

    Ejercicio 6 mejora

--- Manejo_de_exceptiones_con_python/Ejercicio_6.py
+++ Manejo_de_exceptiones_con_python/Ejercicio_6.py
@@ -10,5 +10,17 @@ try:
 except pymysql.err.ProgrammingError:
     print("Error de programación...")
     conexion.close()
+except pymysql.err.InternalError:
+    print("Error interno...")
+except pymysql.err.OperationalError:
+    print("Error de operación...")
+except pymysql.err.IntegrityError:
+    print("Error de integridad...")
+except pymysql.err.DataError:
+    print("Error de datos...")
+except pymysql.err.NotSupportedError:
+    print("Error no soportado...")
+except pymysql.err.DatabaseError:
+    print("Error de base de datos...")
 except:
     print("Error de conexión...")
