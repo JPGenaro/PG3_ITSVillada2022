e73b02d6cd9136c3756df0adee225c3d98fba08a
Juan Pablo Genaro <jpgenaro06@gmail.com>
2022-05-06 08:49:04 -0300

    Ejercicio 6 mejorado

--- Manejo_de_exceptiones_con_python/Ejercicio_6.py
+++ Manejo_de_exceptiones_con_python/Ejercicio_6.py
@@ -1,12 +1,19 @@
 import pymysql
 
-conexion = pymysql.connect( host='localhost', user= 'bdi', passwd='Pepe_123456789', db='Exception')
-cur = conexion.cursor()
-
 try:
-    cur.execute( "se lect nombre, apellido FROM usuarios" )
-    for nombre, apellido in cur.fetchall() :
-        print (nombre, apellido)
+    conexion = pymysql.connect( host='localhost', user= 'bdi', passwd='Pepe_123456789', db='Exception')
+    cur = conexion.cursor()
+    cur.execute("create table if not exists usuarios (id int primary key, nombre varchar(50), apellido varchar(50))")
+    print("Tabla creada")
+    cur.execute("insert into usuarios (id, nombre, apellido) values (1, 'Juan', 'Perez')")
+    cur.execute("insert into usuarios (id, nombre, apellido) values (2, 'Pedro', 'Mondiola')")
+    cur.execute("insert into usuarios (id, nombre, apellido) values (3, 'Ana', 'Gonzalez')")
+    print("Usuarios insertados\n")
+    cur.execute("select * FROM usuarios" )
+    for id, nombre, apellido in cur.fetchall() :
+        print (id, nombre, apellido)
+    cur.execute("drop table usuarios")
+    conexion.close()
 except pymysql.err.ProgrammingError as e:
     print(e, "\nError de programación...")
 except pymysql.err.InternalError as e:
@@ -23,5 +30,3 @@ except pymysql.err.DatabaseError as e:
     print(e, "\nError de base de datos...")
 except Exception as e:
     print(e, "\nError desconocido...")
-
-conexion.close()
